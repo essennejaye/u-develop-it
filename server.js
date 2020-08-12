4e3f85bfd31e5ecc4cb07aa8c76513000695a5d5
essennejaye <sjeffers02@gmail.com>
2020-08-11 17:58:57 -0700

    add post route

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
+const inputCheck = require('./utils/inputCheck');
 const express = require('express');
-const { OPEN_READWRITE } = require('sqlite3');
-const { json } = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const sqlite3 = require('sqlite3').verbose();
@@ -16,6 +15,7 @@ const db = new sqlite3.Database('./db/election.db', err => {
     }
     console.log('Connected to the election database.');
 });
+
 // GET all candidates
 app.get('/api/candidates', (req, res) => {
     const sql = `SELECT * FROM candidates`;
@@ -66,21 +66,33 @@ app.delete('/api/candidates/:id', (req, res) => {
 });
 
 // Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//              VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-// // ES5 function, not arrow function, to use this
-// db.run(sql, params, function (err, result) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result, this.lastID);
-// });
+app.post('/api/candidates', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+             VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use this
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+});
 
-// // Default response for any other request(Not Found) Catch all
-// app.use((req, res) => {
-//     res.status(404).end();
-// });
+// Default response for any other request(Not Found) Catch all
+app.use((req, res) => {
+    res.status(404).end();
+});
 
 // Start server after DB connection
 db.on('open', () => {
